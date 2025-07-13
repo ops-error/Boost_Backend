50dcf6613f25ed0f9be723552b53255697a1a784
EL MOBO <anastasiia.inc@yandex.ru>
2025-07-13 22:15:09 +0300

    comment auth&token middlewares

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,9 +1,18 @@
 const jwt = require('jsonwebtoken');
 const {JWT_TOKEN} = process.env;
+const InvalidAuthError = require('../errors/invalid-auth.err');
 
+// после token middleware
+// читает с headers access токен
 module.exports = (req, res, next) => {
-    const {authorization} = req.headers;
-    const token = authorization.replace('Bearer ', '');
-    req.user = jwt.verify(token, JWT_TOKEN);
-    next();
+    try {
+        const {authorization} = req.headers;
+        const token = authorization.replace('Bearer ', '');
+        // проверяет подпись и в случае успеха => next
+        req.user = jwt.verify(token, JWT_TOKEN);
+        next();
+    } catch {
+        throw new InvalidAuthError('Not Success Auth :(');
+    }
+    
 }
--- middlewares/token.middleware.js
+++ middlewares/token.middleware.js
@@ -4,27 +4,34 @@ const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const {generateAccessToken} = require('../services/token.service');
 
+// проверка на наличие токенов
 module.exports = async (req, res, next) =>{
     const { authorization, firebaseId } = req.headers;
     const { refreshToken } = req.cookies;
 
+    // если оба токена отсутствуют => ошибка
     if (!firebaseId || !refreshToken) {
         throw new UnauthorizedError('Bad request1');
     }
+    // ________________________________________________________
+    // если нет access токена, но есть refresh токен
 
-    if (authorization || authorization.startsWith('Bearer ')) {
-        next();
-    }
-
+    // поиск данных токена через firebaseId
     const tokenData = await RefreshToken.findOne({ firebaseId });
     const isValidToken = bcrypt.compare(refreshToken, tokenData.token);
+    // если хэш refresh токена не совпадает => ошибка
     if (!isValidToken) {
         throw new UnauthorizedError('Bad request2');
     }
-    if (isValidToken && firebaseId && refreshToken && authorization) {
+    // если хэш совпадает & наличие firebaseId &
+    // наличие refresh токена & наличие access токена
+    // => next
+    if (isValidToken && firebaseId && refreshToken && authorization.startsWith('Bearer ')) {
         next();
     }
 
+    // если access токена все же нет, то
+    // создается новый
     const userData = await User.findOne({ firebaseId });
     let newAccessToken;
     if (!authorization || !authorization.startsWith('Bearer ')) {
@@ -32,6 +39,9 @@ module.exports = async (req, res, next) =>{
             userId: userData._id,
             role: userData.role,
         });
+        // отныне access токен записывается в headers
+        // после авторизации клиент тоже записывает его в localStorage,
+        // а оттуда в headers
         req.headers = {
             ...req.headers,
             authorization: `Bearer ${newAccessToken}`
